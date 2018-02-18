bd616a71412796e0be9a5c8260fa1b0d5757df5f
うぃんじー <wing3196@gmail.com>
2018-02-18 21:02:11 +0900

    MIDIIOライブラリの動作確認
    dllがうまくいかなかったのでスタティックライブラリにした

--- Piannect/Main.cpp
+++ Piannect/Main.cpp
@@ -1,9 +1,9 @@
 ﻿#include "StaffNotation.h"
 
-void Main()
-{
-	Graphics2D::SetSamplerState(SamplerState::ClampLinear);
-	Graphics::SetBackground(Palette::White);
+namespace Piannect {
+
+
+void Run() {
 	using namespace Piannect;
 	StaffNotation sn(0, 0, Window::Width(), Window::Height());
 	while (System::Update()) {
@@ -14,5 +14,41 @@ void Main()
 			sn.pushNote(48);
 		}
 		sn.update();
+	}
+}
+
+void MidiioTest() {
+	Console.open();
+	int n = MIDIIn_GetDeviceNum();
+	for (int i = 0; i < n; i++) {
+		wchar_t name[100];
+		MIDIIn_GetDeviceName(i, name, 100);
+		printf("%ls\n", name);
+	}
+}
+
+
+}
+
+void Main()
+{
+	enum RunMode {
+		Run,
+		MidiioTest
+	} runMode = RunMode::MidiioTest;
+
+
+	Graphics2D::SetSamplerState(SamplerState::ClampLinear);
+	Graphics::SetBackground(Palette::White);
+
+	switch (runMode) {
+	case Run:
+		Piannect::Run();
+		break;
+	case MidiioTest:
+		Piannect::MidiioTest();
+		break;
+	default:
+		break;
 	}
 }
--- Piannect/stdafx.h
+++ Piannect/stdafx.h
@@ -3,6 +3,10 @@
 #include <vector>
 #include <list>
 #include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include "MIDIOLib1.1\MIDIIO.h"
 
 
 namespace Piannect {
